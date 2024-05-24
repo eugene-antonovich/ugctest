31b45801c3a5017ed5d4b4f10f8fd113fa6a7809
Eugene <e1.97antonovich@gmail.com>
2024-05-24 15:15:37 +0200

    add map

--- src/components/contact/contact.component.tsx
+++ src/components/contact/contact.component.tsx
@@ -42,6 +42,12 @@ const Contact = () => {
           </div>
         </div>
       </div>
+      <div className={contact.contactMap}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40095.61881328165!2d16.998286996516477!3d51.09044718601567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fc2c870513769%3A0x40fa0c8c9c68bd31!2sLedsy%20Custom%20Neon!5e0!3m2!1sru!2spl!4v1716555365575!5m2!1sru!2spl"
+          loading="lazy"
+        ></iframe>
+      </div>
     </section>
   );
 };
